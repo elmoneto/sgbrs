0c37676ee90b2a786a6f889417f6e10e341c7cd2
Elmo Neto <elmo@inf.ufsm.br>
2020-07-23 19:43:48 -0300

    Configuração de ticks, rótulos e eixos movida para fora da função animate

--- covid19-sgb-rs/curva_covid19_video.py
+++ covid19-sgb-rs/curva_covid19_video.py
@@ -22,17 +22,17 @@ ax.yaxis.grid(True)
 date_form = DateFormatter("%d/%m")
 ax.xaxis.set_major_formatter(date_form)
 line, = ax.plot([], [], lw=2)
+plt.xlabel('Dias',fontsize=10)
+plt.ylabel('Casos',fontsize=10)
+plt.xticks(np.arange(datetime.date(2020,3,31),datetime.date(2020,7,22),step=datetime.timedelta(days=10)))
+plt.yticks(np.arange(0,350,step=25.0))
+plt.xlim([datetime.date(2020,3,31), datetime.date(2020,7,22)])
+plt.ylim(0, 360)
+ax.tick_params(axis='both', which='major', labelsize=8)
 
 def animate(i):    
-    plt.xlabel('Dias',fontsize=10)
-    plt.ylabel('Casos',fontsize=10)
-    ax.tick_params(axis='both', which='major', labelsize=8)
-    plt.xticks(np.arange(datetime.date(2020,3,31),datetime.date(2020,7,22),step=datetime.timedelta(days=10)))
-    plt.yticks(np.arange(0,350,step=25.0))
-    plt.xlim([datetime.date(2020,3,31), datetime.date(2020,7,22)])
-    plt.ylim(0, 360)
     data = coronadados.iloc[:int(i)]
-    linha1,= plt.plot(data.index, data['Confirmados'],label="Confirmados",color="b")
+    linha1,=plt.plot(data.index, data['Confirmados'],label="Confirmados",color="b")
     linha2,=plt.plot(data.index, data['Recuperados'],label="Recuperados",color="g")
     linha3,=plt.plot(data.index, data['Óbitos'],label="Óbitos",color="r")
     plt.legend([linha1,linha2,linha3], ['Confirmados','Recuperados','Óbitos'],loc="upper left")
@@ -41,7 +41,7 @@ def animate(i):
 ani = animation.FuncAnimation(fig, animate, interval=500, repeat=False)
 Writer = animation.writers['ffmpeg']
 writer = Writer(fps=30, metadata=dict(artist='Me'), bitrate=1700)
-print('Renderizando o vídeo com ffmpeg')
+print('Renderizando o vídeo com ffmpeg...')
 ani.save('Covid19SG.mp4', writer=writer)
 
 
